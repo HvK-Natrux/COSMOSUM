c7b749c9331c5d12c08119bea22501bab8e3d337
HvK-Natrux <aurele.lagarde11@gmail.com>
2025-03-02 15:46:46 +0100

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, jsonify, request
 import json
 import os
 from faction_manager import FactionManager
+from threading import Thread
+import time
 
 app = Flask(__name__)
 faction_manager = FactionManager()
@@ -18,5 +20,21 @@ def get_factions():
     factions = faction_manager._load_factions()
     return jsonify(factions)
 
+def run():
+    """Démarre le serveur Flask"""
+    app.run(host='0.0.0.0', port=5000)
+
+def keep_alive():
+    """Démarre le serveur dans un thread séparé"""
+    t = Thread(target=run)
+    t.daemon = True  # Le thread s'arrêtera quand le programme principal s'arrête
+    t.start()
+    return t
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    server_thread = keep_alive()
+    try:
+        while True:
+            time.sleep(1)  # Maintient le thread principal actif
+    except KeyboardInterrupt:
+        print("Arrêt du serveur...")
