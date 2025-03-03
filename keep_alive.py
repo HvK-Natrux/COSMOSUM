75cdf8f03a7a18da4b9f7b6fd8f38679fdf518c4
HvK-Natrux <aurele.lagarde11@gmail.com>
2025-03-03 18:08:23 +0100

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,15 +1,17 @@
+
 from flask import Flask
 from threading import Thread
 
-app = Flask('')
+app = Flask('keep_alive')
 
 @app.route('/')
 def home():
-    return "Le serveur est actif"
+    return "Le bot est actif"
 
 def run():
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
     t = Thread(target=run)
+    t.daemon = True
     t.start()
